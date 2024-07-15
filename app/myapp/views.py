a150fab8e479e8ca4f7d69313c99032a51ffe2ce
Yaroslav Markivskyi <yaroslav.markivskiy.it.2022@lpnu.ua>
2024-07-15 13:23:09 +0300

    feat: Add index view to display all food items

--- app/myapp/views.py
+++ app/myapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Food
 # Create your views here.
+
+
+def index(request):
+    food = Food.objects.all()
+    return render(request, 'food/index.html', {'food': food})
